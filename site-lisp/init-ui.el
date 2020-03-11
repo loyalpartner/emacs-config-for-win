d8cab01dfeb13c5fe8d473cafc7cc3a34383a4fe
凯 <loyalpartner@163.com>
2020-03-11 21:22:23 +0800

    ADD: load different theme by time.

--- site-lisp/init-ui.el
+++ site-lisp/init-ui.el
@@ -31,11 +31,41 @@
   :config (doom-modeline-mode 1))
 
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
+(defvar change-theme-idle (* 30 1))
+
 (use-package doom-themes
   :straight t
   :after-call pre-command-hook
-  :init
-  (add-hook 'after-init-hook (lambda () (load-theme 'doom-one-light t))))
+  ;; :init
+  ;; (add-hook 'after-init-hook (lambda () (load-theme 'doom-one-light t)))
+  :config
+  (change-theme-handler))
+
+(defun current-time-get-hour ()
+  (nth 2 (decode-time)))
+
+(defun current-time-day-p ()
+  (elt (number-sequence 8 20) (current-time-get-hour)))
+
+(defun current-time-night-p ()
+  (not (current-time-day-p)))
+
+(setq change-theme-idle 3)
+(defun change-theme-handler ()
+  "根据时间的不同，显示不同的主题"
+  (let* ((current-theme (car custom-enabled-themes)))
+    (cond ((and (current-time-night-p)
+                (not (equal current-theme 'doom-one)))
+           (load-theme 'doom-one t))
+          ((and (current-time-day-p)
+                (equal current-theme 'doom-one))
+           (load-theme 'doom-one-light t))))) 
+
+;; (eval-after-load 'doom-themes)
+(with-eval-after-load 'doom-themes
+  (run-with-idle-timer change-theme-idle t
+                       #'change-theme-handler))
 
 (use-package which-key :straight t
   :commands which-key-mode
